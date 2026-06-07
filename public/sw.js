6da1141ba0caaec514e752db1cabdc21784d0ebd
Sverrir <sigursveinsson@gmail.com>
2026-06-07 21:41:12 +0000

    PWA: uppsetjanlegt app, tákn og offline

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'tossalisti-v1'
+
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()))
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request
+  if (req.method !== 'GET') return
+  e.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone()
+        caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {})
+        return res
+      })
+      .catch(() => caches.match(req).then((r) => r || caches.match('/')))
+  )
+})
